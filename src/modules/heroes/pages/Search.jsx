a8f371d5bbc17088e46de33381bad4ec00ad8fd2
David Zapata <dzr794@gmail.com>
2024-04-10 14:47:34 -0500

    se avanzo en la funcionalidad de busqueda del heroe

--- src/modules/heroes/pages/Search.jsx
+++ src/modules/heroes/pages/Search.jsx
@@ -1,5 +1,30 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useForm } from "../../../hooks/useForm";
+import { HeroCard } from "../components/HeroCard";
+import queryString from 'query-string';
 
 export const Search = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  const { searchText, onInputChange, onFormReset } = useForm({
+    searchText: '',
+  });
+
+  const { s = '' } = queryString.parse( location.search );
+  console.log({ s });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+
+    const searchQuery = searchText.toLowerCase().trim();
+
+    if (searchQuery.length < 1) return;
+
+    navigate(`?q=${searchQuery}`);
+  }
+
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +34,15 @@ export const Search = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={ onSearchSubmit }>
             <input 
               type="text" 
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={ searchText }
+              onChange={ onInputChange }
             />
             <button className="btn btn-outline-primary mt-1">
               Search
@@ -31,8 +58,10 @@ export const Search = () => {
           </div>
 
           <div className="alert alert-danger">
-            There is no results.
+            There is no results with '{ s }'.
           </div>
+
+          {/* <HeroCard  {...hero} /> */}
 
         </div>
       </div>
